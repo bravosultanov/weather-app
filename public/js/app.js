cd068c96690855f357ac74ea7d70b86a1a4e1c61
Vladimir <bravosultanov@gmail.com>
2021-02-22 10:51:52 +0600

    Added heroku

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,7 @@ form.addEventListener('submit', (e) => {
     const location = search.value;
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
-    fetch(`http://localhost:3000/weather?address=${location}`)
+    fetch(`/weather?address=${location}`)
         .then(res => res.json())
         .then(data => {
             if (data.error) {
